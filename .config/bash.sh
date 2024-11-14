c1ed465f5d7f9b9eb5dcf14f36a1da2bc0f7801d
prabhjotlisp <prabhjotsingh.lisp@gmail.com>
2024-11-15 01:22:29 +0530

    big changes

---
+++ .config/bash.sh
@@ -0,0 +1,10 @@
+alias ls='ls --color=auto'
+alias la='ls -alh --color=auto'
+alias l='ls -lh --color=auto'
+
+alias binc='brightnessctl set 10%+'
+alias bdec='brightnessctl set 10%-'
+
+alias vi='nvim'
+
+PS1='[ \W ]\$ '
